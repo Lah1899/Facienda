9e45e23e8fb784d265d16b40e2c4bb3766c5bbad
Lah <cfiload@gmail.com>
2025-11-07 14:42:49 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Facienda
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Root _root; // 全データを格納するオブジェクト
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadJson();
+        }
+
+        // Jsonからのデータ読み込み
+        private void LoadJson()
+        {
+            var json = File.ReadAllText("facienda.json");
+            _root = JsonConvert.DeserializeObject<Root>(json);
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facienda
+{
+    internal class Root
+    {
+        public List<TaskItem> Tasks { get; set; }
+        public List<ActionItem> Actions { get; set; }
+    }
+
+    public class TaskItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string DueDate { get; set; }
+        public string Note{ get; set; }
+    }
+
+    public class ActionItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+        public string TaskId { get; set; }
+    }
+}
