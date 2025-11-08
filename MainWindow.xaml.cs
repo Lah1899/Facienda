6497bbf43a515c96bfe8fec485902345c9b4317b
Lah <cfiload@gmail.com>
2025-11-08 16:44:11 +0900

    UI作成

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,7 +26,27 @@ namespace Facienda
         public MainWindow()
         {
             InitializeComponent();
-            LoadJson();
+            // _root = LoadJson();
+            _root = new Root {
+                Tasks = new List<TaskItem>
+                {
+                    new TaskItem { Id = "1", Name = "ダミー1", DueDate = "2025-01-01", Note = "テスト" },
+                    new TaskItem { Id = "2", Name = "ダミー2", DueDate = "2025-01-01", Note = "テスト" }
+                },
+                Actions = new List<ActionItem>
+                {
+                    new ActionItem { Id = "a1", Name = "ダミー1のアクション1", IsDone = false, TaskId = "1" },
+                    new ActionItem { Id = "a2", Name = "ダミー1のアクション2", IsDone = true,  TaskId = "1" },
+                    new ActionItem { Id = "a3", Name = "ダミー2のアクション1", IsDone = false, TaskId = "2" }
+                },
+            };
+            _root.Tasks[0].Actions.Add(_root.Actions[0]); // テスト用のダミー処理
+            _root.Tasks[0].Actions.Add(_root.Actions[1]); // テスト用のダミー処理
+            _root.Tasks[1].Actions.Add(_root.Actions[2]); // テスト用のダミー処理
+            DataContext = _root;
+            SidebarTabs.SelectedIndex = 0;
+            ContentTabs.SelectedIndex = 0;
+            // MessageBox.Show(this, _root.Tasks[1].Name);
         }
 
         // Jsonからのデータ読み込み
@@ -34,6 +54,89 @@ namespace Facienda
         {
             var json = File.ReadAllText("facienda.json");
             _root = JsonConvert.DeserializeObject<Root>(json);
+            foreach (ActionItem action in _root.Actions)
+            {
+                foreach (TaskItem task in _root.Tasks)
+                {
+                    if(task.Id == action.TaskId)
+                    {
+                        task.Actions.Add(action);
+                    }
+                }
+            }
         }
+
+        // Jsonへのデータ保存
+        private void SaveJson()
+        {
+
+        }
+
+        // アクションカードの完了ステータス反転
+        private void ToggleActionStatus()
+        {
+
+        }
+
+        // タスクのリネーム
+        private void RenameTask()
+        {
+
+        }
+
+        // アクションのリネーム
+        private void RenameAction()
+        {
+
+        }
+
+        // 完了アクションのクリア
+        private void ClearActions()
+        {
+
+        }
+
+        // タスクの削除
+        private void DeleteTask()
+        {
+
+        }
+
+        // アクションの削除
+        private void DeleteAction()
+        {
+
+        }
+
+        /*
+        // タスクタブの追加
+        private System.Windows.Controls.StackPanel AddTaskTab(TaskItem task)
+        {
+            // 左メニューにタスクを追加
+            // TaskItemオブジェクトを画面項目のDataContextとしてセット
+            var item = new System.Windows.Controls.ListBoxItem { Content = task.Name };
+            item.DataContext = task;
+            SidebarTabs.Items.Add(item);
+
+            // テンプレートからタブ内容を生成
+            var template = (System.Windows.DataTemplate)this.FindResource("TaskTabContentTemplate");
+            var content = (System.Windows.FrameworkElement)template.LoadContent();
+            var scroll = (System.Windows.Controls.ScrollViewer)content;
+            var actionsHost = (System.Windows.Controls.StackPanel)scroll.Content;
+            var tab = new System.Windows.Controls.TabItem { Content = content };
+            ContentTabs.Items.Add(tab);
+
+            return actionsHost;
+        }
+
+        // アクションカードの追加
+        private void AddActionCard(System.Windows.Controls.StackPanel panel, ActionItem action)
+        {
+            var template = (System.Windows.DataTemplate)this.FindResource("ActionCardTemplate");
+            var content = (System.Windows.FrameworkElement)template.LoadContent();
+            content.DataContext = action;
+            panel.Children.Add(content);
+        }
+        */
     }
 }
--- Model.cs
+++ Model.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,19 +15,117 @@ namespace Facienda
         public List<ActionItem> Actions { get; set; }
     }
 
-    public class TaskItem
+    public class TaskItem : INotifyPropertyChanged
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string DueDate { get; set; }
-        public string Note{ get; set; }
+        private string _id;
+        private string _name;
+        private string _dueDate;
+        private string _note;
+
+        public ObservableCollection<ActionItem> Actions { get; } = new ObservableCollection<ActionItem>();
+
+        public string Id
+        {
+            get => _id;
+            set
+            {
+                if (_id == value) return;
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name == value) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string DueDate
+        {
+            get => _dueDate;
+            set
+            {
+                if (_dueDate == value) return;
+                _dueDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Note
+        {
+            get => _note;
+            set
+            {
+                if (_note == value) return;
+                _note = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 
-    public class ActionItem
+    public class ActionItem : INotifyPropertyChanged
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public bool IsDone { get; set; }
-        public string TaskId { get; set; }
+        private string _id;
+        private string _name;
+        private bool _isDone;
+        private string _taskId;
+
+        public string Id
+        {
+            get => _id;
+            set
+            {
+                if (_id == value) return;
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name == value) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                if (_isDone == value) return;
+                _isDone = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string TaskId
+        {
+            get => _taskId;
+            set
+            {
+                if (_taskId == value) return;
+                _taskId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
